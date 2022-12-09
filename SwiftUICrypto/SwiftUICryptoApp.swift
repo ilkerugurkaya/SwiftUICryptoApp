dec8d735129e857aed58d11a25c61bf44917af3d
ilkerugurkaya <kayailkerugur@gmail.com>
2022-12-09 21:13:05 +0300

    Initial Commit

---
+++ SwiftUICrypto/SwiftUICryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoApp.swift
+//  SwiftUICrypto
+//
+//  Created by İlker Kaya on 9.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
